2cb27f717198f6deab618f70bbcd644a4b7e14de
Sergeev Dima <sergeev@tbm.ru>
2023-01-31 14:36:32 +0300

    fixed bugs

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 'use strict';
 
-let title = 'Проект Урок 2',
-  screens = 'Простые, Сложные, Интерактивные',
-  screenPrice = 550,
-  rollback = 80,
-  fullPrice = 4000,
-  adaptive = true;
+const rollback = 80;
+const title = prompt('Как называется ваш проект?');
+const screens = prompt('Какие типы экранов нужно разработать?');
+const screenPrice = prompt('Сколько будет стоить данная работа?');
+const adaptive = confirm('Нужен ли адаптив на сайте?');
+const service1 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice1 = prompt('Сколько это будет стоить?');
+const service2 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice2 = prompt('Сколько это будет стоить?');
+const fullPrice = +screenPrice + +servicePrice1 + +servicePrice2;
 
-alert('Hello!');
-
-console.log('Hello in console!');
+const servicePercentPrice = Math.ceil(fullPrice - (rollback * fullPrice / 100));
 
 console.log(`type title: ${typeof title}, type fullPrice: ${typeof fullPrice}, type adaptive: ${typeof adaptive}`);
 
@@ -23,26 +25,14 @@ console.log(screens.toLowerCase().split(', '));
 
 console.log('Процент отката посреднику за работу: ', fullPrice * (rollback / 100));
 
-title = prompt('Как называется ваш проект?');
-screens = prompt('Какие типы экранов нужно разработать?');
-screenPrice = prompt('Сколько будет стоить данная работа?');
-adaptive = confirm('Нужен ли адаптив на сайте?');
-
-const service1 = prompt('Какой дополнительный тип услуги нужен?');
-const servicePrice1 = prompt('Сколько это будет стоить?');
-const service2 = prompt('Какой дополнительный тип услуги нужен?');
-const servicePrice2 = prompt('Сколько это будет стоить?');
-
-fullPrice = +screenPrice + +servicePrice1 + +servicePrice2;
-
-const servicePercentPrice = Math.ceil(fullPrice - rollback);
-
 console.log('servicePercentPrice = ', servicePercentPrice);
 
-if (fullPrice >= 30000) { // поставил >= т.к. не учитывается стоимость 30000 (ровно)
+if (fullPrice >= 30000) {
   console.log('Даем скидку в 10%');
-} else if (fullPrice >= 15000 && fullPrice < 30000) {
+} else if (fullPrice >= 15000) {
   console.log('Даем скидку в 5%');
-} else if (fullPrice < 15000 && fullPrice > 0) {
+} else if (fullPrice < 15000) {
   console.log('Скидка не предусмотрена');
+} else if (fullPrice < 0) {
+  console.log('Что то пошло не так');
 }
